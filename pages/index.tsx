575e88ff54275597fded1f9c61b42ba8eecc73ee
Hau Chen <cettoana@gmail.com>
2021-01-07 23:11:54 +0800

    Next.js init

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Container } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  // TODO
+}));
+
+function AniSearch() {
+  const classes = useStyles();
+
+  return <Container>{/* TODO */}</Container>;
+}
+
+export default AniSearch;
